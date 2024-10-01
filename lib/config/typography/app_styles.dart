8a049795007fddaa2cd9663f7460d70b3397f174
vishnu prasad kp <vishnuprasad.mukundan@gmail.com>
2024-10-01 15:10:48 +0530

    Improve readability in app_styles.dart
    minor readability improvement

--- lib/config/typography/app_styles.dart
+++ lib/config/typography/app_styles.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
-
+ 
 class AppStyles {
-  static TextStyle style34 =
-      const TextStyle(fontWeight: FontWeight.w400, fontSize: 34);
-  static TextStyle style20 =
-      const TextStyle(fontWeight: FontWeight.w400, fontSize: 20);
-  static TextStyle style18 =
-      const TextStyle(fontWeight: FontWeight.w400, fontSize: 18);
-  static TextStyle style16 =
-      const TextStyle(fontWeight: FontWeight.w400, fontSize: 16);
-  static TextStyle style14 =
-      const TextStyle(fontWeight: FontWeight.w400, fontSize: 14);
-  static TextStyle style12 =
-      const TextStyle(fontWeight: FontWeight.w400, fontSize: 12);
-  static TextStyle style10 =
-      const TextStyle(fontWeight: FontWeight.w400, fontSize: 10);
-  static TextStyle style9 =
-      const TextStyle(fontWeight: FontWeight.w400, fontSize: 9);
-  static TextStyle style8 =
-      const TextStyle(fontWeight: FontWeight.w400, fontSize: 8);
-  static TextStyle style6 =
-      const TextStyle(fontWeight: FontWeight.w400, fontSize: 6);
+  static TextStyle textStyle(double size, {FontWeight weight = FontWeight.w400}) {
+    return TextStyle(fontSize: size, fontWeight: weight);
+  }
+ 
+  static TextStyle style34 = textStyle(34);
+  static TextStyle style20 = textStyle(20);
+  static TextStyle style18 = textStyle(18);
+  static TextStyle style16 = textStyle(16);
+  static TextStyle style14 = textStyle(14);
+  static TextStyle style12 = textStyle(12);
+  static TextStyle style10 = textStyle(10);
+  static TextStyle style9 = textStyle(9);
+  static TextStyle style8 = textStyle(8);
+  static TextStyle style6 = textStyle(6);
 }
